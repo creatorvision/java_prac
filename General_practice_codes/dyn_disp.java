4d1fbced01d38eda41207686ebb869ea3b27c7e2
creatorvision <creatorvision03@gmail.com>
2014-08-27 13:26:56 +0530

    ADD GPC/DD.java

---
+++ General_practice_codes/dyn_disp.java
@@ -0,0 +1,40 @@
+//@creatorvision
+import java.util.*;
+import java.io.*;
+
+class dyn_disp{
+
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner (System.in);
+		String str =sc.nextLine();
+		System.out.println();
+		display(str);
+
+	}
+
+	public static void display(String s){	
+
+			for (int i=0;i<s.length();i++ ) 
+			{
+				System.out.print(s.substring(i));
+				System.out.println(s.substring(0,i));
+					
+				try {
+				Runtime.getRuntime().exec("clear");
+				} catch(IOException ioe){
+					ioe.printStackTrace();
+				}
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException ex) {
+					Thread.currentThread().interrupt();
+				}
+
+				final String ANSI_CLS = "\u001b[2J";
+		        final String ANSI_HOME = "\u001b[H";
+		        System.out.print(ANSI_CLS + ANSI_HOME);
+		        System.out.flush();
+			}
+	}
+}
